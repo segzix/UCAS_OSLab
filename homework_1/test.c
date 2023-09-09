e1c577d449cc849034c9b6b726cd65f1478066ab
Shengzixuan <chengzixuan21a@mails.ucas.ac.cn>
2023-09-09 14:57:10 +0000

    commit message

---
+++ homework_1/test.c
@@ -0,0 +1,60 @@
+#define COUNT 10
+#include <time.h>
+#include <stdio.h>
+#include <sys/syscall.h>
+#include <sys/time.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+int var = 100;      //全局变量
+
+int main(void)
+{
+    int i;
+
+    pid_t pid_1;
+    pid_t pid_2;
+    pid_t pid_3;
+
+    double diff_1 = 0;
+    double diff_2 = 0;
+    double diff_3 = 0;
+
+    struct timespec time1;
+    struct timespec time2;
+    struct timespec time3;
+    struct timespec time4;
+    struct timespec time5;
+    struct timespec time6;
+
+    for(i = 0;i < COUNT;i++)
+    {
+
+        clock_gettime(CLOCK_REALTIME, &time1);
+        pid_1 = getpid(); 
+        clock_gettime(CLOCK_REALTIME, &time2);
+
+        clock_gettime(CLOCK_REALTIME, &time3);
+        pid_2 = syscall(SYS_getpid);
+        clock_gettime(CLOCK_REALTIME, &time4);
+
+        clock_gettime(CLOCK_REALTIME, &time5);
+        asm volatile(
+            "mov $20, %%eax\n\t"
+            "syscall\n\t"
+            : "=a"(pid_3)
+        );
+        clock_gettime(CLOCK_REALTIME, &time6);
+
+        diff_1 += time2.tv_nsec - time1.tv_nsec;
+        diff_2 += time4.tv_nsec - time3.tv_nsec;
+        diff_3 += time6.tv_nsec - time5.tv_nsec;
+    }
+
+    printf("time_getpid = %f ns\n", diff_1/COUNT);
+    printf("time_syscall = %f ns\n", diff_2/COUNT);
+    printf("time_syscall_getpid = %f ns\n", diff_3/COUNT);
+
+    return 0;
+}
+
